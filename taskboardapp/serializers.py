e93898c1390c7b7c016f0fa74fb25a83eafda60b
Idel Yusupov <idel84@mail.ru>
2021-12-19 21:20:45 +0300

    Lesson_4

--- taskboardapp/serializers.py
+++ taskboardapp/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework.serializers import ModelSerializer
-from usersapp.serializers import UserSerializer
+from rest_framework.serializers import ModelSerializer, StringRelatedField
+# from usersapp.serializers import UserSerializer
 from .models import WorkProject, TaskBoard
 
 
@@ -10,7 +10,7 @@ class WorkProjectSerializer(ModelSerializer):
 
 
 class TaskBoardSerializer(ModelSerializer):
-    user = UserSerializer()
+    user = StringRelatedField()
 
     class Meta:
         model = TaskBoard
--- taskboardapp/views.py
+++ taskboardapp/views.py
@@ -1,15 +1,50 @@
-from django.http import HttpResponse
-from rest_framework.renderers import JSONRenderer
-from rest_framework.viewsets import ModelViewSet
+# from django.http import HttpResponse
+# from rest_framework.decorators import renderer_classes
+from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, \
+    UpdateModelMixin, CreateModelMixin, DestroyModelMixin
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from taskboardapp.models import WorkProject, TaskBoard
 from taskboardapp.serializers import WorkProjectSerializer, TaskBoardSerializer
 
 
 class WorkProjectViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = WorkProject.objects.all()
     serializer_class = WorkProjectSerializer
 
+    def get(self, request, *args, **kwargs):
+        if 'pk' in kwargs:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
 
-class TaskBoardViewSet(ModelViewSet):
+
+# class WorkProjectParamFilterViewSet(ModelViewSet):
+#     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+#     queryset = WorkProject.objects.all()
+#     serializer_class = WorkProjectSerializer
+#
+#     def get_queryset(self):
+#         name = self.request.query_params.get('name', '')
+#         projects = WorkProject.objects.all()
+#         if name:
+#             projects = projects.filter(name__contains=name)
+#         return projects
+    # def get_queryset(self, name=None):
+    #     return WorkProject.objects.filter(name=self.request.query_params.get(name))
+
+# class WorkProjectViewSet(ModelViewSet):
+#     queryset = WorkProject.objects.all()
+#     serializer_class = WorkProjectSerializer
+
+
+class TaskBoardViewSet(ListModelMixin, GenericViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = TaskBoard.objects.all()
     serializer_class = TaskBoardSerializer
+
+# class TaskBoardViewSet(ModelViewSet):
+#     queryset = TaskBoard.objects.all()
+#     serializer_class = TaskBoardSerializer
